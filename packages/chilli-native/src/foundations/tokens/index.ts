67c98972d07002be1c049d78bb75920daf8a2d4a
Lucas Simula <lucas.simula@gmail.com>
2026-04-17 17:28:54 +0200

    feat(native): add primitive tokens barrel

---
+++ packages/chilli-native/src/foundations/tokens/index.ts
@@ -0,0 +1,25 @@
+export { colors } from './colors';
+export type { Colors } from './colors';
+export { spacing } from './spacing';
+export type { Spacing } from './spacing';
+export { radius } from './radius';
+export type { Radius } from './radius';
+export { borderWidth } from './borderWidth';
+export type { BorderWidth } from './borderWidth';
+export { size } from './size';
+export type { Size } from './size';
+export {
+  fontSize,
+  lineHeight,
+  letterSpacing,
+  fontFamily,
+  textStyles,
+} from './typography';
+export type {
+  FontSize,
+  LineHeight,
+  LetterSpacing,
+  FontFamily,
+  TextStyles,
+  TextStyleVariant,
+} from './typography';
